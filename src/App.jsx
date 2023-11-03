8583486243e54ed3365f586c4e768beb494a321d
tilok <tilakthexp@gmail.com>
2023-11-03 13:24:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='parent'>
+    <div className='container'>
+      <div className='heading'>
+        <h1>
+        Keep Notes</h1>  
+      </div>     
+      <div className='description'>
+        A web application to keep all your reminders and important notes safe on cloud
+      </div>
+      <div className='features'>
+        Integrating Amazon AWS for safekeeping of all your notes
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
